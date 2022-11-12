0a6110bbbf913a26c12701ad91a2e534cd601cb5
Aaron <bauhd@gmx.net>
2022-11-12 15:27:20 +0100

    server: send all player info at once

--- server/src/main/java/de/bauhd/minecraft/server/protocol/packet/play/PlayerInfo.java
+++ server/src/main/java/de/bauhd/minecraft/server/protocol/packet/play/PlayerInfo.java
@@ -2,104 +2,86 @@ package de.bauhd.minecraft.server.protocol.packet.play;
 
 import de.bauhd.minecraft.server.DefaultMinecraftServer;
 import de.bauhd.minecraft.server.api.entity.MinecraftPlayer;
-import de.bauhd.minecraft.server.api.entity.player.GameProfile;
 import de.bauhd.minecraft.server.protocol.Protocol;
 import de.bauhd.minecraft.server.protocol.packet.Packet;
 import io.netty5.buffer.Buffer;
-import net.kyori.adventure.text.Component;
 
 import java.util.List;
-import java.util.UUID;
 
 import static de.bauhd.minecraft.server.protocol.packet.PacketUtils.*;
 
 public final class PlayerInfo implements Packet {
 
-    private int action;
-    private UUID uniqueId;
-    private String name;
-    private List<GameProfile.Property> properties;
-    private int gameMode;
-    private int ping;
-    private Component displayName;
+    private final int action;
+    private final List<MinecraftPlayer> players;
 
-    private PlayerInfo(final int action, final UUID uniqueId, final String name, final List<GameProfile.Property> properties,
-                       final int gameMode, final int ping, final Component displayName) {
+    public PlayerInfo(final int action, final List<MinecraftPlayer> players) {
         this.action = action;
-        this.uniqueId = uniqueId;
-        this.name = name;
-        this.properties = properties;
-        this.gameMode = gameMode;
-        this.ping = ping;
-        this.displayName = displayName;
-    }
-
-    public PlayerInfo() {}
-
-    @Override
-    public void decode(Buffer buf, Protocol.Version version) {
-
+        this.players = players;
     }
 
     @Override
     public void encode(Buffer buf, Protocol.Version version) {
         writeVarInt(buf, this.action);
-        writeVarInt(buf, 1);
-        writeUUID(buf, this.uniqueId);
+        writeVarInt(buf, this.players.size());
+        for (final var player : this.players) {
+            writeUUID(buf, player.getUniqueId());
 
-        if (this.action == 0) {
-            writeString(buf, this.name);
-            writeVarInt(buf, this.properties.size());
-            System.out.println(this.properties);
-            for (final var property : this.properties) {
-                writeString(buf, property.key());
-                writeString(buf, property.value());
-                if (property.signature() != null) {
+            if (this.action == 0) {
+                writeString(buf, player.getUsername());
+                writeVarInt(buf, player.getProfile().properties().size());
+                for (final var property : player.getProfile().properties()) {
+                    writeString(buf, property.key());
+                    writeString(buf, property.value());
+                    if (property.signature() != null) {
+                        buf.writeBoolean(true);
+                        writeString(buf, property.signature());
+                    } else {
+                        buf.writeBoolean(false);
+                    }
+                }
+                // TODO add game mode and ping
+                writeVarInt(buf, 0);
+                writeVarInt(buf, 1);
+                if (player.getDisplayName() != null) {
                     buf.writeBoolean(true);
-                    writeString(buf, property.signature());
+                    writeString(buf, DefaultMinecraftServer.getGsonSerializer(version).serialize(player.getDisplayName()));
                 } else {
                     buf.writeBoolean(false);
                 }
-            }
-            writeVarInt(buf, this.gameMode);
-            writeVarInt(buf, this.ping);
-            if (this.displayName != null) {
-                buf.writeBoolean(true);
-                writeString(buf, DefaultMinecraftServer.getGsonSerializer(version).serialize(this.displayName));
-            } else {
                 buf.writeBoolean(false);
+            } else if (this.action == 1) {
+
+            } else if (this.action == 2) {
+
+            } else if (this.action == 3) {
+
             }
-            buf.writeBoolean(false);
-        } else if (this.action == 1) {
-
-        } else if (this.action == 2) {
-
-        } else if (this.action == 3) {
 
         }
     }
 
-    public static PlayerInfo add(final UUID uniqueId, final String name, final List<GameProfile.Property> properties, final int gameMode, final int ping, final Component displayName) {
-        return new PlayerInfo(0, uniqueId, name, properties, gameMode, ping, displayName);
+    public static PlayerInfo add(List<MinecraftPlayer> players) {
+        return new PlayerInfo(0, players);
     }
 
-    public static PlayerInfo add(final MinecraftPlayer player) {
-        return PlayerInfo.add(player.getUniqueId(), player.getUsername(), player.getProfile().properties(), 1, 1, null);
+    public static PlayerInfo add(MinecraftPlayer player) {
+        return add(List.of(player));
     }
 
-    public static PlayerInfo remove(final UUID uniqueId) {
-        return new PlayerInfo(4, uniqueId, null, null, -1, -1, null);
+    public static PlayerInfo remove(List<MinecraftPlayer> players) {
+        return new PlayerInfo(4, players);
+    }
+
+    public static PlayerInfo remove(MinecraftPlayer player) {
+        return add(List.of(player));
     }
 
     @Override
     public String toString() {
         return "PlayerInfo{" +
                 "action=" + this.action +
-                ", uniqueId=" + this.uniqueId +
-                ", name='" + this.name + '\'' +
-                ", gameMode=" + this.gameMode +
-                ", ping=" + this.ping +
-                ", displayName=" + this.displayName +
+                ", players=" + this.players +
                 '}';
     }
 }
