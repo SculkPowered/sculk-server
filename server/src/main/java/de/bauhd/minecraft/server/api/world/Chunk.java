80802c33c98d9755a10a5157b184ad5466ea4453
Aaron <bauhd@gmx.net>
2022-11-25 23:58:41 +0100

    server: moved heightmaps to Dimension

--- server/src/main/java/de/bauhd/minecraft/server/api/world/Chunk.java
+++ server/src/main/java/de/bauhd/minecraft/server/api/world/Chunk.java
@@ -1,10 +1,10 @@
 package de.bauhd.minecraft.server.api.world;
 
 import de.bauhd.minecraft.server.api.entity.MinecraftPlayer;
+import de.bauhd.minecraft.server.api.world.dimension.Dimension;
 import de.bauhd.minecraft.server.protocol.packet.play.ChunkDataAndUpdateLight;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import net.kyori.adventure.nbt.CompoundBinaryTag;
 
 import java.util.ArrayList;
 import java.util.BitSet;
@@ -39,27 +39,13 @@ public final class Chunk {
     }
 
     public void send(MinecraftPlayer player) {
-        final var dimensionHeight = 384;
-        final var motionBlocking = new int[256];
-        final var worldSurface = new int[256];
-        for (var x = 0; x < 16; x++) {
-            for (var z = 0; z < 16; z++) {
-                motionBlocking[x + z * 16] = 0;
-                worldSurface[x + z * 16] = dimensionHeight - 1;
-            }
-        }
-        final var bitsForHeight = Integer.SIZE - Integer.numberOfLeadingZeros(dimensionHeight);
-        final var heightmaps = CompoundBinaryTag.builder()
-                .putLongArray("MOTION_BLOCKING", this.encodeBlocks(motionBlocking, bitsForHeight))
-                .putLongArray("WORLD_SURFACE", this.encodeBlocks(worldSurface, bitsForHeight))
-                .build();
         final var emptyBitSet = new BitSet();
         final var emptyLight = BitSet.valueOf(new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24});
 
         player.send(new ChunkDataAndUpdateLight(
                 this.chunkX,
                 this.chunkZ,
-                heightmaps,
+                Dimension.OVERWORLD.heightmaps(),
                 new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                         16, 0, 0, 9, 0, 0, 0, 0, 16, 0, 0, 9, 0, 0, 0, 0, 8, 0, 4, 2, 0, 9, -128, 2, 17, 17, 17, 17, 17, 17,
                         17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
@@ -144,50 +130,5 @@ public final class Chunk {
                 emptyLight,
                 emptyLight)
         );
-    }
-
-    // TODO
-    private static final int[] MAGIC = {
-            -1, -1, 0, Integer.MIN_VALUE, 0, 0, 1431655765, 1431655765, 0, Integer.MIN_VALUE,
-            0, 1, 858993459, 858993459, 0, 715827882, 715827882, 0, 613566756, 613566756,
-            0, Integer.MIN_VALUE, 0, 2, 477218588, 477218588, 0, 429496729, 429496729, 0,
-            390451572, 390451572, 0, 357913941, 357913941, 0, 330382099, 330382099, 0, 306783378,
-            306783378, 0, 286331153, 286331153, 0, Integer.MIN_VALUE, 0, 3, 252645135, 252645135,
-            0, 238609294, 238609294, 0, 226050910, 226050910, 0, 214748364, 214748364, 0,
-            204522252, 204522252, 0, 195225786, 195225786, 0, 186737708, 186737708, 0, 178956970,
-            178956970, 0, 171798691, 171798691, 0, 165191049, 165191049, 0, 159072862, 159072862,
-            0, 153391689, 153391689, 0, 148102320, 148102320, 0, 143165576, 143165576, 0,
-            138547332, 138547332, 0, Integer.MIN_VALUE, 0, 4, 130150524, 130150524, 0, 126322567,
-            126322567, 0, 122713351, 122713351, 0, 119304647, 119304647, 0, 116080197, 116080197,
-            0, 113025455, 113025455, 0, 110127366, 110127366, 0, 107374182, 107374182, 0,
-            104755299, 104755299, 0, 102261126, 102261126, 0, 99882960, 99882960, 0, 97612893,
-            97612893, 0, 95443717, 95443717, 0, 93368854, 93368854, 0, 91382282, 91382282,
-            0, 89478485, 89478485, 0, 87652393, 87652393, 0, 85899345, 85899345, 0,
-            84215045, 84215045, 0, 82595524, 82595524, 0, 81037118, 81037118, 0, 79536431,
-            79536431, 0, 78090314, 78090314, 0, 76695844, 76695844, 0, 75350303, 75350303,
-            0, 74051160, 74051160, 0, 72796055, 72796055, 0, 71582788, 71582788, 0,
-            70409299, 70409299, 0, 69273666, 69273666, 0, 68174084, 68174084, 0, Integer.MIN_VALUE,
-            0, 5
-    };
-
-    private long[] encodeBlocks(int[] blocks, int bitsPerEntry) {
-        final var maxEntryValue = (1L << bitsPerEntry) - 1;
-        final var valuesPerLong = (char) (64 / bitsPerEntry);
-        final var magicIndex = 3 * (valuesPerLong - 1);
-        final var divideMul = Integer.toUnsignedLong(MAGIC[magicIndex]);
-        final var divideAdd = Integer.toUnsignedLong(MAGIC[magicIndex + 1]);
-        final var divideShift = MAGIC[magicIndex + 2];
-        final var size = (blocks.length + valuesPerLong - 1) / valuesPerLong;
-
-        var data = new long[size];
-
-        for (var i = 0; i < blocks.length; i++) {
-            final var value = blocks[i];
-            final var cellIndex = (int) (i * divideMul + divideAdd >> 32L >> divideShift);
-            final var bitIndex = (i - cellIndex * valuesPerLong) * bitsPerEntry;
-            data[cellIndex] = data[cellIndex] & ~(maxEntryValue << bitIndex) | (value & maxEntryValue) << bitIndex;
-        }
-
-        return data;
     }
 }
