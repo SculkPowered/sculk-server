b75af53b1e6dc8be2849adffd327e4350279b495
Aaron <bauhd@gmx.net>
2023-04-09 23:30:27 +0200

    server: move light bitsets to static fields

--- server/src/main/java/de/bauhd/minecraft/server/api/world/chunk/MinecraftChunk.java
+++ server/src/main/java/de/bauhd/minecraft/server/api/world/chunk/MinecraftChunk.java
@@ -17,6 +17,10 @@ import java.util.List;
 
 public final class MinecraftChunk implements Chunk {
 
+    private static final BitSet EMPTY_BIT_SET = new BitSet();
+    private static final BitSet EMPTY_LIGHT = BitSet.valueOf(
+            new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24});
+
     private final Dimension dimension;
     private final int x;
     private final int z;
@@ -52,19 +56,16 @@ public final class MinecraftChunk implements Chunk {
     }
 
     public void send(MinecraftPlayer player) {
-        final var emptyBitSet = new BitSet();
-        final var emptyLight = BitSet.valueOf(new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24});
-
         player.send(new ChunkDataAndUpdateLight(
                 this.x,
                 this.z,
                 Dimension.OVERWORLD.heightmaps(),
                 this.sectionsToData(),
                 true,
-                emptyBitSet,
-                emptyBitSet,
-                emptyLight,
-                emptyLight)
+                EMPTY_BIT_SET,
+                EMPTY_BIT_SET,
+                EMPTY_LIGHT,
+                EMPTY_LIGHT)
         );
     }
 
