6295f7f2406e0c45c9175155527a587c77eedbfc
Aaron <bauhd@gmx.net>
2023-06-22 19:18:08 +0200

    build(javdoc): add adventure's nbt to javadoc

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -16,6 +16,7 @@ tasks {
         (options as StandardJavadocDocletOptions).links = listOf(
             "https://docs.oracle.com/en/java/javase/17/docs/api/",
             "https://jd.advntr.dev/api/${libs.versions.adventure.get()}/",
+            "https://jd.advntr.dev/nbt/${libs.versions.adventure.get()}/",
             "https://jd.advntr.dev/text-logger-slf4j/${libs.versions.adventure.get()}/"
         )
     }
