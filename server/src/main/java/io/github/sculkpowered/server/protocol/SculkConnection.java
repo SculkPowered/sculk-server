491b32f217eb381bbb2d6fd4d84069fb1927d64f
Aaron <bauhd@gmx.net>
2024-04-12 21:30:18 +0200

    chore: send chunk waiting info

--- server/src/main/java/io/github/sculkpowered/server/protocol/SculkConnection.java
+++ server/src/main/java/io/github/sculkpowered/server/protocol/SculkConnection.java
@@ -41,6 +41,7 @@ import io.github.sculkpowered.server.protocol.packet.handler.StatusPacketHandler
 import io.github.sculkpowered.server.protocol.packet.login.CompressionPacket;
 import io.github.sculkpowered.server.protocol.packet.login.LoginDisconnect;
 import io.github.sculkpowered.server.protocol.packet.login.LoginSuccess;
+import io.github.sculkpowered.server.protocol.packet.play.GameEvent;
 import io.github.sculkpowered.server.protocol.packet.play.Login;
 import io.github.sculkpowered.server.protocol.packet.play.PlayerInfo;
 import io.github.sculkpowered.server.protocol.packet.play.PlayerInfoRemove;
@@ -233,6 +234,7 @@ public final class SculkConnection extends ChannelInboundHandlerAdapter implemen
               world.dimension().name()));
 
           this.sendCommands();
+          this.send(new GameEvent(13, -1));
           this.player.calculateChunks(position, position, false, false);
           this.send(new SpawnPosition(position));
           this.send(new SynchronizePlayerPosition(position));
