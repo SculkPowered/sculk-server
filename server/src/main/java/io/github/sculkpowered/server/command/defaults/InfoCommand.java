c27e68ca58ac0b0c55881e6d2ba5c9487ff2d1a1
Aaron <bauhd@gmx.net>
2024-06-02 12:01:58 +0200

    chore: add utility for commands

--- server/src/main/java/io/github/sculkpowered/server/command/defaults/InfoCommand.java
+++ server/src/main/java/io/github/sculkpowered/server/command/defaults/InfoCommand.java
@@ -3,12 +3,12 @@ package io.github.sculkpowered.server.command.defaults;
 import static net.kyori.adventure.text.Component.text;
 
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.sun.management.OperatingSystemMXBean;
 import io.github.sculkpowered.server.Server;
 import io.github.sculkpowered.server.SculkServer;
 import io.github.sculkpowered.server.command.CommandSource;
+import io.github.sculkpowered.server.command.Commands;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import net.kyori.adventure.text.Component;
@@ -19,7 +19,7 @@ public final class InfoCommand {
   private static final MemoryMXBean MEMORY_MX_BEAN = ManagementFactory.getMemoryMXBean();
 
   public static LiteralCommandNode<CommandSource> get() {
-    return LiteralArgumentBuilder.<CommandSource>literal("info")
+    return Commands.literal("info")
         .requires(commandSource -> commandSource.hasPermission("server.command.info"))
         .executes(context -> {
           final var heapMemoryUsage = MEMORY_MX_BEAN.getHeapMemoryUsage();
--- server/src/main/java/io/github/sculkpowered/server/command/defaults/StopCommand.java
+++ server/src/main/java/io/github/sculkpowered/server/command/defaults/StopCommand.java
@@ -1,15 +1,15 @@
 package io.github.sculkpowered.server.command.defaults;
 
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.github.sculkpowered.server.SculkServer;
 import io.github.sculkpowered.server.command.CommandSource;
+import io.github.sculkpowered.server.command.Commands;
 
 public final class StopCommand {
 
   public static LiteralCommandNode<CommandSource> get(final SculkServer server) {
-    return LiteralArgumentBuilder.<CommandSource>literal("stop")
+    return Commands.literal("stop")
         .requires(commandSource -> commandSource.hasPermission("server.command.stop"))
         .executes(context -> {
           server.shutdown();
