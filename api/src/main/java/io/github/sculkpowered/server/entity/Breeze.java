2f491b97e946502cba887035f1d4185239bacf4b
Aaron <bauhd@gmx.net>
2024-08-07 00:16:23 +0200

    chore: update registries

--- api/src/main/java/io/github/sculkpowered/server/entity/Breeze.java
+++ api/src/main/java/io/github/sculkpowered/server/entity/Breeze.java
@@ -1,6 +1,6 @@
 package io.github.sculkpowered.server.entity;
 
-import org.jetbrains.annotations.ApiStatus.Experimental;
-
-@Experimental // 1.21
+/**
+ * Represents a breeze entity.
+ */
 public interface Breeze extends Entity {}
--- api/src/main/java/io/github/sculkpowered/server/entity/WindCharge.java
+++ api/src/main/java/io/github/sculkpowered/server/entity/WindCharge.java
@@ -1,6 +1,6 @@
 package io.github.sculkpowered.server.entity;
 
-import org.jetbrains.annotations.ApiStatus.Experimental;
-
-@Experimental // 1.21
+/**
+ * Represents a wind charge entity.
+ */
 public interface WindCharge extends Entity {}
