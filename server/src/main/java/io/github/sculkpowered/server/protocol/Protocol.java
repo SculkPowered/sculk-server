d0cc514e9cac4d874373915b89a09a8ee5ae5991
Aaron <bauhd@gmx.net>
2024-09-11 01:07:29 +0200

    chore: 1.21.1

--- server/src/main/java/io/github/sculkpowered/server/protocol/Protocol.java
+++ server/src/main/java/io/github/sculkpowered/server/protocol/Protocol.java
@@ -2,6 +2,6 @@ package io.github.sculkpowered.server.protocol;
 
 public final class Protocol {
 
-  public static final String VERSION_NAME = "1.21";
+  public static final String VERSION_NAME = "1.21.1";
   public static final int VERSION_PROTOCOL = 767;
 }
